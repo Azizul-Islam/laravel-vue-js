fbb5d2ca073e6f1ec1822d1b28a2e0982cc6705c
Azizul Islam <azizul.islam@LEADSOFT.BIZ>
2022-10-13 16:49:04 +0600

    first commit

---
+++ app/Http/Controllers/Api/SkillController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SkillRequest;
+use App\Http\Resources\SkillCollection;
+use App\Http\Resources\SkillResource;
+use App\Models\Skill;
+use Illuminate\Http\Request;
+
+class SkillController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return new SkillCollection(Skill::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(SkillRequest $request)
+    {
+        Skill::create($request->validated());
+        return response()->json('Skill created success');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Skill  $skill
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Skill $skill)
+    {
+        return new SkillResource($skill);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Skill  $skill
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Skill $skill)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Skill  $skill
+     * @return \Illuminate\Http\Response
+     */
+    public function update(SkillRequest $request, Skill $skill)
+    {
+        $skill->update($request->validated());
+        return response()->json('Skill updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Skill  $skill
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Skill $skill)
+    {
+        $skill->delete();
+        return response()->json('Skill Deleted');
+    }
+}
